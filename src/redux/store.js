f4a786a72947744fdba197bae5666c9302bbe1c1
AKLukman <abulkashemlukman@gmail.com>
2023-01-12 22:33:58 +0600

    currying function and redux middleweares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, composeWithDevTools());
+composeWithDevTools();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(cartCounter, logger, thunk))
+);
 
 export default store;
